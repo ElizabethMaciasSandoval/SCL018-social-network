5ff078ac040a1d7918d01928ac433069570c849c
SaraRiverosOlmedo <s.riveros.olmedo@gmail.com>
2021-11-19 18:10:54 -0300

    arreglo del codigo del ruteo

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 // Este es el punto de entrada de tu aplicacion
-import { myFunction } from './lib/index.js';
-import { drawLogin } from './lib/pages/templateLogin.js';
+import { myFunction } from './lib/firestore.js';
 import { changeRoute } from './lib/routes.js';
 
 myFunction();
-document.getElementById("root").innerHTML+=drawLogin();
+
 const init = () =>{
+    window.location.hash="#/";
     window.addEventListener("hashchange", () =>{
         changeRoute(window.location.hash);
     });
