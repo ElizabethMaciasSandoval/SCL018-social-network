341060008cabddbe9ed822cbcf6a889e6308dae9
ElizabethMaciasSandoval <elizabeth130394@gmail.com>
2021-11-25 18:25:44 -0300

    aplicamos estilo a la pagina de login

--- src/pages/templateLogin.js
+++ src/pages/templateLogin.js
@@ -6,19 +6,20 @@ export const drawLogin = () => {
     <section>
       <section class = "containerWelcome">
       <section class = "firstPage">
-        <h1>¡¡BIENVENIDOS A ESTUDIANDO!!</h1>
-        <h2>¿Qué es EstudiAndo?</h2>
-        <p>Es una App Web creada para que estudiantes compartan material educativo, en la cual encontrarán
+        <img src="img/logo-estudiando2.png" alt="logo" id="imgEtudiando" class="img-estudiando">
+        <p class="Login-Message">Es una App Web creada para que estudiantes compartan material educativo, en la cual encontrarán
         información, recursos, links y temas relacionados con la PTU, orientación vocacional y emocional,
         sobre el espectro de carreras profesional, etc.
         </p>
+        <img src="img/Img1.1.png" alt="mujer estudiando" class="woman-img">
+        <i class="fas fa-angle-double-down"></i>
       </section>
       </section>
       <section class = "containerLogin">
-        <img src="" alt="" id="">
-        <input type="email" id="loginMail" placeholder="Ingresa tu correo electrónico">
-        <input type="password" id="loginPassword" placeholder="Ingresa tu clave">
-        <button id="loginButton">Ingresa</button>
+        <h1 class="Title-Login">Inicia Sesión</h1>
+        <input type="email" id="loginMail" class="Inputs-Login" placeholder="Ingresa tu correo electrónico">
+        <input type="password" id="loginPassword" class="Inputs-Login" placeholder="Ingresa tu clave">
+        <button id="loginButton" class="Button-Login">Ingresa</button>
         <p>O</p>
         <img src="img/logo-google.png" alt="LogoGoogle" id="logoGoogle">
         <p>¿No tienes cuenta?</p>
@@ -39,7 +40,7 @@ export const drawLogin = () => {
 
   const googleButton = draw.querySelector('#logoGoogle');
   googleButton.addEventListener('click', () => {
-  console.log('Holiwi');
+    console.log('Holiwi');
     googleLogin();
   });
   return draw;
