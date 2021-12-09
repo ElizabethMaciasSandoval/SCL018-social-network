7485a2b343383484d175847f7c33b4a5e057642c
SaraRiverosOlmedo <s.riveros.olmedo@gmail.com>
2021-12-09 12:43:11 -0300

    Se cambió el modal por un textarea, se trabajó en el diseño del templateWall, agregando atributo y se agrego una condicion para poder publicar

--- src/pages/templateWall.js
+++ src/pages/templateWall.js
@@ -9,54 +9,40 @@ export const drawWall = () => {
   const draw = document.createElement('div');
   const templatePageWall = `
     <section class="wall-container">
-    <section class="headerNav">
+     <section class="headerNav">
       <header>
         <img src="img/Logo-oficial_00.png" alt="logo" id="imgEtudiando" class="img-estudiando">
       </header>
       <nav class ="navWall">
         <img src="img/log-out.png" alt="cierre de sesión" class="log-out" id="logOut">
-        <img src="img/writing.png" alt="crear post" class="new-post" id="newPost">
       </nav>
-    </section>
-    <section class= "post-container" id="postContainer">
-    </section>
+      </section>
+     <section class="container-new-post" id="containerNewPost">
+       <textarea class="text-post" id="textPost" cols="50" rows="5" placeholder="Escribe aquí..."></textarea>
+       <button class="btn-publicar" id="btnPublicar">Publicar</button>
+     </section>
+     <section class= "post-container" id="postContainer">
+     </section>
     </section>
   `;
+
   draw.innerHTML = templatePageWall;
 
-  //Template del modal para crear publicaciones
-  const drawModal = document.createElement('div');
-  const templateModalNewPost = `
-  <section class="container-new-post" id="containerNewPost">
-    <img src="img/cross.png" alt="cierre de modal" class="close-modal" id="closeModal">
-    <textarea class="text-post" id="textPost" cols="30" rows="10"></textarea>
-    <button class="btn-publicar" id="btnPublicar">Publicar</button>
-  </section>
-  `;
-  drawModal.innerHTML = templateModalNewPost;
-
-  //Funciones del pageWall y funciones del modal
-  const buttonNewPost = draw.querySelector('#newPost');
-  buttonNewPost.addEventListener('click', () => {
-    console.log('hola a todes');
-    drawModal.innerHTML = templateModalNewPost;
-    const buttonCloseModal = drawModal.querySelector('#closeModal');
-    buttonCloseModal.addEventListener('click', () => {
-      draw.innerHTML = templatePageWall;
-    });
-    const buttonPublicar = drawModal.querySelector('#btnPublicar');
-    buttonPublicar.addEventListener('click', () => {
-      const textPost = drawModal.querySelector('#textPost').value;
-      console.log(textPost);
-      console.log('hola a todos');
+  // Funciones del pageWall y funciones del modal
+  const buttonPublicar = draw.querySelector('#btnPublicar');
+  buttonPublicar.addEventListener('click', () => {
+    const textPost = draw.querySelector('#textPost').value;
+    if (textPost === '') {
+      alert('Escribe tu publicación, por favor');
+    } else {
       addDataPost(textPost);
-      drawModal.querySelector('#textPost').value = '';
-      draw.innerHTML = templatePageWall;
-    });
+      draw.querySelector('#textPost').value = '';
+    }
   });
+
   postView();
 
-  //Botón de cierre de sesión
+  // Botón de cierre de sesión
   const buttonOutIcon = draw.querySelector('#logOut');
   buttonOutIcon.addEventListener('click', () => {
     console.log('okey');
